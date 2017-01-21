47cd54a37184c6b694cd1f2b120ce7082eaf1824
shubham9411 <shubham9411@gmail.com>
2017-01-21 12:59:40 +0530

    WP follow

--- public/wp-content/themes/Mr-Robot/footer.php
+++ public/wp-content/themes/Mr-Robot/footer.php
@@ -9,5 +9,8 @@
 <!-- <?php echo get_num_queries(); ?> queries in <?php timer_stop(3); ?> seconds -->
 <div class="bar" role="bar"></div>
 <?php wp_footer(); ?>
+<script type="text/javascript" >
+	(function(d){var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');p.type = 'text/javascript';p.async = true;p.src = '//widgets.wp.com/platform.js';f.parentNode.insertBefore(p,f);}(document));
+</script>
 </body>
 </html>
